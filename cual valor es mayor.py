a3ff26fb275790520a8a82547a1621d62729a0de
matizurdo <matizurdo2015@gmail.com>
2025-04-24 17:25:17 -0300

    Add files via upload

---
+++ cual valor es mayor.py
@@ -0,0 +1,9 @@
+valor1 = float(input("ingrese el primer valor"))
+valor2 = float(input("ingrese el segundo valor"))
+
+if valor1 > valor2:
+    print("el primer valor es mayor")
+elif valor1 < valor2:
+    print("el segundo valor es mayor")
+else:
+    print("los valores son iguales")
